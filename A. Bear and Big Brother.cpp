ed4ab745bc02b6a3838b6dba1348b8c4bf9c2660
AmitKhan012 <uchihaamit1@gmail.com>
2020-06-02 00:39:33 +0600

    Create A. Bear and Big Brother.cpp

---
+++ A. Bear and Big Brother.cpp
@@ -0,0 +1,13 @@
+# include <cstdio>
+ 
+int main()
+{
+     int a, b, year = 0;
+     scanf("%d %d", &a, &b);
+     while(a <= b){
+           a *= 3;
+           b *= 2;
+          year++;
+    }
+    printf("%d\n", year);
+}
